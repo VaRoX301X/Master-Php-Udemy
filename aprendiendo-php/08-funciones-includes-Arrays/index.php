9bf092549f4ab430cd567becb104be93d10174b8
Alvaro Recalde <elvarox301@gmail.com>
2019-03-07 22:50:52 +0100

    changes

---
+++ aprendiendo-php/08-funciones-includes-Arrays/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * FUNCIONES:
+ * conjunto de instrucciones agrupadas bajo un nombre concreto y que pueden reutilizarse  tantas veces como se desee
+ */
+function nombre_de_funcion(){
+    echo 'FUNCIONES';
+}
+nombre_de_funcion();
+/**
+ * Created by PhpStorm.
+ * User: varox
+ * Date: 07/02/2019
+ * Time: 18:11
+ */
