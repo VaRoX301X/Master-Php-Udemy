67845cd399bd826e56256cf3ed3cad0a95e444ac
Alvaro Recalde <elvarox301@gmail.com>
2019-10-24 19:20:41 +0200

    Ver entradas por categoría

--- blog1/includes/helpers.php
+++ blog1/includes/helpers.php
@@ -39,6 +39,15 @@ function conseguirCategorias($conexion) {
 
     return $resultado;
 }
+function conseguirCategoria($conexion, $id) {
+    $sql = "SELECT * FROM categorias WHERE id = $id;";
+    $categorias = mysqli_query($conexion, $sql);
+    $resultado = array();
+    if ($categorias && mysqli_num_rows($categorias) >= 1) {
+        $resultado = mysqli_fetch_assoc($categorias);
+    }
+    return $resultado;
+}
 
 function conseguirEntradas($conexion, $limit = null, $categoria = null, $busqueda = NULL) {
     $sql = "SELECT e.*, c.nombre AS 'categoria' FROM entradas e "
