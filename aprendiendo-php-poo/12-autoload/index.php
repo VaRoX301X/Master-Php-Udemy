4be4b8f32f0730f519db47599e8989f3ea4655a8
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-10-29 18:03:13 +0100

    Comprobar existencia de clases y metodos

--- aprendiendo-php-poo/12-autoload/index.php
+++ aprendiendo-php-poo/12-autoload/index.php
@@ -31,6 +31,54 @@ class Principal {
     public $categoria;
     public $entrada;
 
+    /**
+     * @return Usuario
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param Usuario $usuario
+     */
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    /**
+     * @return Categoria
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * @param Categoria $categoria
+     */
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+    }
+
+    /**
+     * @return \MisClases\Entrada
+     */
+    public function getEntrada()
+    {
+        return $this->entrada;
+    }
+
+    /**
+     * @param \MisClases\Entrada $entrada
+     */
+    public function setEntrada($entrada)
+    {
+        $this->entrada = $entrada;
+    }
+
     public function __construct()
     {
         $this->usuario = new Usuario();
@@ -42,3 +90,17 @@ $principal = new Principal();
 var_dump($principal);
 $usuario = new userAdmin();
 var_dump($usuario);
+
+
+// Ver los metodos que existen
+var_dump(get_class_methods($principal));
+
+//Podemos hacer un array_search para buscar si existe un metodo
+
+// Comprobar si existe una clase
+$clase_existe = @class_exists("PanelAdministrador\Usuario");
+if($clase_existe) {
+    echo "<h1>La clase SI existe</h1>";
+} else {
+    echo "<h1>La clase NO existe</h1>";
+}
