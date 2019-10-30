2cdf234f75b60a0ef644e5df55ad941ded8b37eb
Alvaro Recalde <elvarox301@gmail.com>
2019-10-30 20:45:38 +0100

    Estructura del MVC

---
+++ aprendiendo-php-mvc/models/usuario.php
@@ -0,0 +1,77 @@
+<?php
+
+
+class Usuario {
+    public $nombre;
+    public $apellidos;
+    public $email;
+    public $password;
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param mixed $apellidos
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function conseguirTodos(){
+        echo "Sacando todos los usuarios";
+    }
+}
