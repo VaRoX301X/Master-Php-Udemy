516ebea51568bc3873b33a4b1ccd37771b7b6467
alvaro.recalde.alguacil <elvarox301@gmail.com>
2019-12-16 12:07:10 +0100

    generar pdf - librerias php

---
+++ aprendiendo-librerias-php/generar-pdf/index.php
@@ -0,0 +1,14 @@
+<?php
+require '../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+$html2pdf = new Html2Pdf();
+
+//Recoger la vista a imprimir
+ob_start();
+require_once  'pdf_para_generar.php';
+$html = ob_get_clean();
+
+$html2pdf->writeHTML($html);
+$html2pdf->output('pdf_generado.pdf');
---
+++ aprendiendo-librerias-php/generar-pdf/pdf_para_generar.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Generar pdf</title>
+    <style type="text/css">
+        body {
+            font-family: Helvetica;
+            background-color: #31F190;
+        }
+        h1 {
+            background-color: rebeccapurple;
+        }
+        p {
+            font-size: 23px;
+        }
+        li {
+            background-color: #007ee5;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hola mundo</h1>
+    <p>Cursos de php</p>
+    <ul>
+        <li>Desarrollo en Js</li>
+        <li>Master en php</li>
+        <li>Desarrollo en xxdebugi</li>
+    </ul>
+</body>
+</html>
